a9b16bdc6a3f5d3fc63e95384e358b55f0ba038b
rr665 <hrrishikesh07@gmail.com>
2024-12-26 10:37:28 +0530

    hrishi

---
+++ lib/hrishikesh.dart
@@ -0,0 +1,13 @@
+class Hrishi extends StatefulWidget {
+  const Hrishi({super.key});
+
+  @override
+  State<Hrishi> createState() => _HrishiState();
+}
+
+class _HrishiState extends State<Hrishi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
