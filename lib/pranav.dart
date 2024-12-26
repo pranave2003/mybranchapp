dce94260e65bcaacf6fbeb607b5bfaa356a5795d
pranave2003 <pranav2003a10@gmail.com>
2024-12-26 09:27:09 +0530

    second

---
+++ lib/pranav.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Pranavwork extends StatefulWidget {
+  const Pranavwork({super.key});
+
+  @override
+  State<Pranavwork> createState() => _PranavworkState();
+}
+
+class _PranavworkState extends State<Pranavwork> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
